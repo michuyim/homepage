a0d93334ff3c86b5a4056ffb73cb334c16527885
Michuyim <118578558+michuyim@users.noreply.github.com>
2024-02-28 11:04:35 +0100

    Add files via upload

---
+++ VAl/val.js
@@ -0,0 +1,22 @@
+const siteData = [
+    {
+        category: "Productivity",
+        links: [
+            { name: "Gmail", url: "https://mail.google.com" },
+            { name: "GoDaddy", url: "https://sso.godaddy.com" },
+            { name: "Facebook", url: "https://facebook.com/wihyunyi" },
+            { name: "Cloudflare", url: "https://dash.cloudflare.com" },
+            { name: "Wordpress", url: "https://wordpress.com" },
+        ]
+    },
+
+    {
+        category: "Web Development",
+        links: [
+            { name: "GitHub", url: "https://github.com" },
+            { name: "Gitlab", url: "https://gitlab.com" },
+
+        ]
+    }
+    // ... other categories ...
+];
